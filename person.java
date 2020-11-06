7fd07634011e4a87e2d7ac5f243c6cb58bc9fb71
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-06 14:30:00 +0000

    class exercise for names and ages

---
+++ person.java
@@ -0,0 +1,17 @@
+package geometry;
+
+public class person {
+    String name;
+    public String getName() {
+        return name;
+    }
+    int age;
+    public int getAge() {
+        return age;
+    }
+
+    public person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
